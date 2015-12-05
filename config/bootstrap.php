8d92f4fc0d7b484d4dd263dfb312b568bffac749
David Yang <daworld.ny@gmail.com>
2015-12-05 15:36:51 +0500

    Composer-compliant change

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -12,8 +12,7 @@
 // Make paths relative to the root folder
 chdir(dirname(__DIR__));
 
-// Include application factory
-require(dirname(__DIR__).'/vendor/Krystal/Application/AppFactory.php');
+require('vendor/autoload.php');
 
 // Return prepared application's instance
-return Krystal\Application\AppFactory::build(require(__DIR__.'/app.php'));
+return \Krystal\Application\AppFactory::build(require(__DIR__.'/app.php'));
