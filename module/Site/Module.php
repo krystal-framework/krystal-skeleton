be6bcd583c5e084ba2aea41c73b49b0d8a0cec68
David Yang <daworld.ny@gmail.com>
2015-12-13 14:35:58 +0500

    Replaced tabs with whitespaces

--- module/Site/Module.php
+++ module/Site/Module.php
@@ -6,33 +6,33 @@ use Krystal\Application\Module\AbstractModule;
 
 final class Module extends AbstractModule
 {
-	/**
-	 * Returns routes of this module
-	 * 
-	 * @return array
-	 */
-	public function getRoutes()
-	{
-		return array(
-			'/' => array(
-				'controller' => 'Welcome@indexAction'
-			),
+    /**
+     * Returns routes of this module
+     * 
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return array(
+            '/' => array(
+                'controller' => 'Welcome@indexAction'
+            ),
 
-			'/hello/(:var)' => array(
-				'controller' => 'Welcome@helloAction',
-			)
-		);
-	}
+            '/hello/(:var)' => array(
+                'controller' => 'Welcome@helloAction',
+            )
+        );
+    }
 
-	/**
-	 * Returns prepared service instances of this module
-	 * 
-	 * @return array
-	 */
-	public function getServiceProviders()
-	{
-		return array(
-		
-		);
-	}
+    /**
+     * Returns prepared service instances of this module
+     * 
+     * @return array
+     */
+    public function getServiceProviders()
+    {
+        return array(
+        
+        );
+    }
 }
