465ec56929f0560277e5bc7ffe7d0378ec1465d6
dave-yang <daworld.ny@gmail.com>
2019-09-27 03:23:09 +0500

    Added getPk() and findByToken() methods

--- module/Site/Storage/MySQL/RecoveryMapper.php
+++ module/Site/Storage/MySQL/RecoveryMapper.php
@@ -13,4 +13,23 @@ final class RecoveryMapper extends AbstractMapper
     {
         return 'users_recovery';
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getPk()
+    {
+        return 'id';
+    }
+
+    /**
+     * Finds an entry by its token
+     * 
+     * @param string $token
+     * @return array
+     */
+    public function findByToken($token)
+    {
+        return $this->fetchByColumn('token', $token);
+    }
 }
