be82974ae6b66f406f44adc888d411b2a5072a0f
dave-yang <daworld.ny@gmail.com>
2019-09-28 18:33:03 +0500

    Added GenderCollection

---
+++ module/Site/Collection/GenderCollection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Site\Collection;
+
+use Krystal\Stdlib\ArrayCollection;
+
+final class GenderCollection extends ArrayCollection
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $collection = array(
+        '0' => 'Prefer not to say',
+        '1' => 'Male',
+        '2' => 'Female'
+    );
+}
