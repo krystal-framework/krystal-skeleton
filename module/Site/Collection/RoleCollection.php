70a19f8d31a993329d2423f04851fc0e7252e193
dave-yang <daworld.ny@gmail.com>
2019-10-01 22:27:38 +0500

    Added RoleCollection

---
+++ module/Site/Collection/RoleCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Site\Collection;
+
+use Krystal\Stdlib\ArrayCollection;
+
+final class RoleCollection extends ArrayCollection
+{
+    /* Most common roles */
+    const ROLE_USER = 1;
+    const ROLE_ADMIN = 2;
+    const ROLE_MODERATOR = 3;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $collection = array(
+        self::ROLE_USER => 'User',
+        self::ROLE_ADMIN => 'Administrator',
+        self::ROLE_MODERATOR => 'Moderator'
+    );
+}
