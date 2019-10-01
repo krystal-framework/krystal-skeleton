0a1ee71e24b929a636dc56120130a0a6d8754db9
dave-yang <daworld.ny@gmail.com>
2019-10-01 22:31:29 +0500

    Removed login column

--- module/Site/Storage/MySQL/schema.sql
+++ module/Site/Storage/MySQL/schema.sql
@@ -5,7 +5,6 @@
 
 CREATE TABLE `users` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `login` varchar(255) NOT NULL COMMENT 'User login',
     `password` varchar(255) NOT NULL COMMENT 'Password hash',
     `role` varchar(10) NOT NULL COMMENT 'Role constant',
     `email` varchar(255) NOT NULL COMMENT 'User email',
